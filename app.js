2ef0dd8336f044ab56e4d9f5aaab60eea94e816d
Matthew Frost <m.frost@mattronix.nl>
2015-05-31 18:23:08 +0200

    First Versopn

---
+++ app.js
@@ -0,0 +1,43 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var URL = 'http://techinc.nl/space/spacestate.json';
+
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Space State',
+  subtitle:'Fetching...'
+});
+// Display the Card
+card.show();
+
+
+// the request 
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log('Successfully fetched Space State!');
+    var state = "";
+    
+    if(data.open) {state = "open";}
+    else if(data.open === false){state = "Closed";}
+    else{state = "ERROR";}
+
+    // show to user 
+    card.subtitle(data.space);   
+    card.icon('tilogo');
+    card.body("space is currently " + state);
+  },
+  
+  function(error) {
+    // Failure!
+    console.log('Failed fetching Space State: ' + error);
+  }
+  
+);
+
+
+
