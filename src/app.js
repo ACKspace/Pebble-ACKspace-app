9249e133ed3d89415f909d7e01dfc1d69d15702e
Vicarious <Vicarious84@users.noreply.github.com>
2015-06-23 21:39:55 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 var UI = require('ui');
 var ajax = require('ajax');
-var URL = 'http://techinc.nl/space/spacestate.json';
+var URL = 'http://ackspace.nl/status.php';
 
 // Create a Card with title and subtitle
 var card = new UI.Card({
-  title:'Space State',
+  title:'Hackerspace',
   subtitle:'Fetching...'
 });
 // Display the Card
@@ -20,15 +20,29 @@ ajax(
   function(data) {
     // Success!
     console.log('Successfully fetched Space State!');
-    var state = "";
+    var status = "";
     
-    if(data.open === true) {state = "open";}
-    else if(data.open === false){state = "Closed";}
-    else{state = "ERROR";}
+    if(data.state.open === true) {status = "Open";}
+    else if(data.state.open === false){status = "Closed";}
+    else{status = "ERROR";}
+    
+    // get lastchanged date + time
+    function timeConverter(UNIX_timestamp){
+        var a = new Date(UNIX_timestamp*1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+    }
 
     // show to user 
     card.subtitle(data.space);   
-    card.body("space is currently " + state);
+    card.body("Status: " + status + "\nChanged: " + timeConverter(data.state.lastchange));
   },
   
   function(error) {
